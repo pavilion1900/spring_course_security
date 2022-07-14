509deaf338942a9c7e28266257f8947133845304
Korolenko Sergey <pavilion1900@gmail.com>
2022-07-14 20:57:03 +0300

    4. Процедура авторизации

--- src/main/java/ru/clevertec/security/configuration/MySecurityConfig.java
+++ src/main/java/ru/clevertec/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.clevertec.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -16,5 +17,14 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("elena").password("elena").roles("HR"))
                 .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info").hasRole("MANAGER")
+                .and().formLogin().permitAll();
     }
 }
